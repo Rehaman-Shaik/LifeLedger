381e358d778153210763d1b318b2e37895a6d3d3
unknown <shaik.rehamanrp@gmail.com>
2024-06-25 21:42:10 +0530

    removed the in house routes added to routes to a main route and streamlines the code

--- server/server.js
+++ server/server.js
@@ -1,41 +1,20 @@
 import cors from "cors"
 import bodyParser from "body-parser";
 import express from "express";
-import getAllUsers from "./models/usersModel.js";
 import financeRouter from "./routes/financeRoute.js";
+import mainRouter from "./routes/mainRoute.js";
 
 const app = express();
 const port = 3000;
 
-//routes
-app.use("/", financeRouter)
-// app.use("/main", router)
-
-
+app.use(express.static("public"))
 app.use(cors());
 app.use(bodyParser.json());
-
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
 
-app.get("/", async (req, res) => {
-    var users = await getAllUsers()
-    res.json({ "users": users })
-})
-
-app.get("/movies", async (req, res) => {
-    res.json({ "movies": "movies" })
-})
-
-app.get("/series", async (req, res) => {
-    res.json({ "series": "series" })
-})
-
-app.get("/games", async (req, res) => {
-    res.json({ "games": "games" })
-})
-
-
-export { app } 
+//routes
+app.use("/", financeRouter)
+app.use("/", mainRouter)
